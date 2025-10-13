1ae4d857ab25b32b650265cded19e4a992f14afc
Diego <139392443+DaigoS3mpai@users.noreply.github.com>
2025-10-13 00:05:32 -0300

    ACtualizacion del  buil.grandle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,6 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.compose.foundation)
-    implementation(libs.transportation.consumer)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -66,6 +65,5 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.6")
     implementation("androidx.compose.material:material-icons-extended")
-    implementation("androidx.compose.material3:material3-window-size-class")
-
+    implementation("androidx.compose.material3:material3-window-size-class:1.3.1")
 }
