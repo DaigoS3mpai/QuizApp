a350aa852755218ac574c9385cb20468148d8d21
Vicente <131227667+haruxvi@users.noreply.github.com>
2025-10-13 09:35:29 -0300

    experimento calcular pantalla provisional

--- app/src/main/java/com/example/quizapp/ui/screen/CalcularTamPantalla.kt
+++ app/src/main/java/com/example/quizapp/ui/screen/CalcularTamPantalla.kt
@@ -1,2 +1,39 @@
 package com.example.quizapp.ui.screen
 
+import androidx.compose.material3.Text
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+import com.example.quizapp.ui.utils.obtenerWindowSizeClass
+
+@Composable
+fun CalcularTamPantalla(screenName: String) {
+    val windowSizeClass = obtenerWindowSizeClass()
+    when (windowSizeClass.widthSizeClass) {
+        WindowWidthSizeClass.Compact -> callScreen(screenName)
+        WindowWidthSizeClass.Medium -> callScreen(screenName)
+        WindowWidthSizeClass.Expanded -> callScreen(screenName + "Expand")
+    }
+}
+
+@Composable
+fun callScreen(screenName: String) {
+    when (screenName) {
+        "Categoria" -> Categoria(navController = rememberNavController())
+        "CategoriaExpand" -> CategoriaExpand(navController = rememberNavController())
+        "Login" -> Login(navController = rememberNavController())
+        "LoginExpand" -> LoginExpand(navController = rememberNavController())
+        "MenuInicioSesion" -> MenuInicioSesion(navController = rememberNavController())
+        "MenuInicioSesionExpand" -> MenuInicioSesionExpand(navController = rememberNavController())
+        "MenuOpciones" -> MenuOpciones(navController = rememberNavController())
+        "MenuOpcionesExpand" -> MenuOpcionesExpand(navController = rememberNavController())
+        "Perfil" -> Perfil(navController = rememberNavController())
+        "PerfilExpand" -> PerfilExpand(navController = rememberNavController())
+        "Registro" -> Registro(navController = rememberNavController())
+        "RegistroExpand" -> RegistroExpand(navController = rememberNavController())
+        "Seleccion" -> Selecion(navController = rememberNavController())
+        "SeleccionExpand" -> SeleccionExpand(navController = rememberNavController())
+        else -> Text("Pantalla no encontrada: $screenName")
+    }
+}
+
